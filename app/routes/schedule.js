c76c5fc12bfddc6f96cc70b418c3dc21a624de4a
Andrey <andrey.prudnikov@qnomy.com>
2022-11-22 17:02:13 +0200

    fix routes

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -2,14 +2,10 @@ import {createLesson, getLessons} from "../controllers/lessons.js";
 
 const getSchedule = (fastify, option, done) => {
     const handlers = {
-        get: async (reply, data) => {
-            return await getLessons(data)
-        },
-        create: async (reply, data) => {
-            return await createLesson(data)
-        }
+        get: async (reply, data) => await getLessons(data),
+        create: async (reply, data) => await createLesson(data)
     }
-    fastify.post('/api/lessons/', {preValidation: fastify.auth}, async (request, reply) => {
+    fastify.post('/api/lessons', {preValidation: fastify.auth}, async (request, reply) => {
         const {user_id, role} = fastify.jwt.decode(request.headers.authorization.split(' ').pop())
         const {method, data} = request.body
         const result = await handlers[method](reply, {...data, user_id, role})
